0f31dc2e88ca002bc2730a47d2a346a8140d51e0
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-18 21:02:10 +0300

    This commit adds slider to the News section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function(){
+	$('.news__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		dots: true,
+	  });
+  });
